debb471fa78181cd3818e338acd1bda66e861ad9
ivan <ivanwolyniec@gmail.com>
2019-10-31 13:01:31 -0400

    button shadow

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Grab the deck element
+const deckEl = document.querySelector(".deck");
+
+// Grab the card elements
+const cardsArray = document.querySelectorAll(".card");
+
+// Attach listener to deck that runs flip on click
+
+// flip function:
+// Checks which face is up (data attribute)
+// Performs rotations on the front and back elements accordingly:
+// face up: face rotates
